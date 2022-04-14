20abb1ea53b688ee7ca8119d180c3701ccc60285
matheus <mgomes494@gmail.com>
2022-04-14 01:09:18 -0300

    Jogos aleatórios gerados

--- script.js
+++ script.js
@@ -5,20 +5,41 @@ const get = (element) => document.querySelector(element)
 const textArea = get('#teamList')
 
 // Trigger
-const btnConclude = get("#btnConclude")
+const btnConclude = get('#btnConclude')
 
 // Armazenando os times e suas respectivas cidades
-const teamList = []
+let teamList = []
 
 btnConclude.addEventListener('click', () => {
+    teamList = ''
+
     const infoTeams = textArea.value.split('\n')
 
-    infoTeams.map(team => teamList.push(team.trim()))
+    teamList = []
+    infoTeams.map((team) => teamList.push(team.trim()))
 
-    console.log("   🚀 ~ file: script.js ~ line 9 ~ teamList", teamList)
+    // console.log(teamList)
 
-    teamList.map(info => {
-        const separator = info.split(";")
-        console.log("🚀 ~ file: script.js ~ line 20 ~ btnConclude.addEventListener ~ info", `${separator[0]} vs ${separator[0]}` )
-    })
+    showRivals()
 })
+
+function showRivals() {
+    let team = teamList.map((team) => team.split(';')[0])
+    let region = teamList.map((team) => team.split(';')[1])
+
+    console.log(team)
+    console.log(region)
+
+    const getRandomPosition = (mult) => Math.round(Math.random() * mult)
+
+    let limit = team.length
+
+    console.log(limit)
+
+    console.log(
+        `${team[getRandomPosition(limit)]} vs ${
+            team[getRandomPosition(limit -= 1)]
+        } - ${region[getRandomPosition(limit)]} - Rodada ${getRandomPosition(1) + 1}
+`
+    )
+}
